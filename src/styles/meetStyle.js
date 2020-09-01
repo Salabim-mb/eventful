05d2ffcec081ecc1090340bc075a55852dc052d4
balasm <01142551@pw.edu.pl>
2020-09-01 22:50:52 +0200

    added meet first layout

---
+++ src/styles/meetStyle.js
@@ -0,0 +1,12 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "100%",
+    },
+    media: {
+        width: "100%",
+        height: "50vw",
+        objectFit: "contain"
+    }
+}));
